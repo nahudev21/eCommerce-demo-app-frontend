947750e2d8b133079bf505d8736910da5eca1172
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-11-02 20:36:32 -0300

    Implementacion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import AdminPage from './pages/AdminPage';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
 
   return (
     <>
-      <h1>Shopping Cart</h1>
+      <Navbar /> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin-page" element={<AdminPage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from '../../assets/TuchinDeals-300x300.png'
+
+export default function Navbar() {
+  return (
+    <nav>
+        <section>
+          <div>
+            <a href='/'>
+              <img src={logo} accessKey='Logo de empresa' /> 
+            </a>
+          </div>
+          <div>
+            <form>
+                <label></label>
+                <input />
+            </form>
+          </div>
+        </section>
+        
+    </nav>
+  )
+}
