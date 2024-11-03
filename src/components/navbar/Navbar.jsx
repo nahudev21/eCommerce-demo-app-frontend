f31d66ffddffbf1279a3ecfe1543a2e61b7fa1d5
Nahuel de la Torre <nahueldelatorre16@gmail.com>
2024-11-03 19:21:06 -0300

    Primer modelo del navbar sin funcionalidad agregada

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,40 @@
-import logo from '../../assets/TuchinDeals-300x300.png'
+import './Navbar.css'
+import { IoMdSearch } from "react-icons/io";
+import { SiShortcut } from "react-icons/si";
+
 
 export default function Navbar() {
   return (
-    <nav>
-        <section>
-          <div>
-            <a href='/'>
-              <img src={logo} accessKey='Logo de empresa' /> 
-            </a>
-          </div>
-          <div>
-            <form>
-                <label></label>
-                <input />
-            </form>
-          </div>
-        </section>
-        
+    <nav className="nav_container">
+      <section className="nav_section-buscador">
+        <div className="nav_logo-container">
+          <a href="/">
+            <SiShortcut className="nav_logo" />
+            <span className="nav_logo-span-1">Tuchin</span>
+            <span className="nav_logo-span-2">Deals</span>
+          </a>
+        </div>
+      </section>
+      <section className="nav_section-button">
+        <div className="nav_container-buscador">
+          <form className="buscador">
+            <input
+              className="buscador_input"
+              name="buscador"
+              placeholder="Buscar..."
+            />
+            <span>
+              <IoMdSearch className="nav_icon" />
+            </span>
+          </form>
+        </div>
+        <div className="nav_container-botones">
+          <button className="nav_button">Regístrate</button>
+        </div>
+        <div>
+          <button className="nav_button">Inicia Sesión</button>
+        </div>
+      </section>
     </nav>
-  )
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react"
 
 export default function Home() {
+  
+  //Prueba con el servidor
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const id = 2
+
+  const product = {
+    name: "Iphone 15 Pro Max",
+    brand: "Apple",
+    price: 2000000,
+    inventory: 10,
+    description: "Iphone 15 Pro Max, 512GB, Negro",
+    category: "Electronics",
+  };
+
+  const user = {
+    email: "nahuel23@gmail.com",
+    password: "admin",
+  };
+
+  const login = async () => {
+    const response = await fetch("http://localhost:9193/api/v1/auth/login", {
+      method: "POST",
+      headers: { "Content-type": "Application/json" },
+      body: JSON.stringify(user),
+    });
+    if(response.ok) {
+      const json = await response.json();
+      console.log(json);
+    }
+  }
+
+  
+
   return (
     <div>
       <h1>Home Page</h1>
